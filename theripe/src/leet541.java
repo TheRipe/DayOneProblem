e363675684974d596be7ae41545c5138b468a563
TheRipe <theripe666@163.com>
2021-08-20 23:43:48 +0800

    力扣每日一题

---
+++ theripe/src/leet541.java
@@ -0,0 +1,24 @@
+/**
+ * @Author TheRipe
+ * @create 2021/8/20 23:43
+ */
+class leet541 {
+    public String reverseStr(String s, int k) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(arr, i, Math.min(i + k, n) - 1);
+        }
+        return new String(arr);
+    }
+
+    public void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
